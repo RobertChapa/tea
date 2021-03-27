0bba788d54464568de0f0045bc84c8638987aba6
RobertChapa <36498804+RobertChapa@users.noreply.github.com>
2021-03-27 17:06:05 -0500

    Update sofar.java

--- sofar.java
+++ sofar.java
@@ -17,7 +17,180 @@ public class Gam
 		int myInteger = scan.nextInt();
 		
 		if(myInteger == 1)
-			System.out.println("Main Story");
+		{
+			System.out.println("\"Yup. Guess the Grand witch is dead.\"");
+			myLine = scan.nextLine();
+			System.out.println("\"The who?\"");
+			myLine = scan.nextLine();
+			System.out.println("\"The Grand Witch. Dust. Puddle.\"");
+			myLine = scan.nextLine();
+			System.out.println("\"?\"");
+			myLine = scan.nextLine();
+			System.out.println("\"Remember? We went to that day camp that one year and we got split it because I was older? and you went with the other kids to play witch. They told us all about the Grand Witch, the Order, and how magic was real and we even got to try riding brooms?\"");
+			myLine = scan.nextLine();
+			System.out.println("\"All we did was play dress up and pretend to cast spells with fake wands! And whats this about magic?\"");
+			myLine = scan.nextLine();
+			System.out.println("\"Yeah, like they went over it. Simple magic, advanced magic, magic cheats, etc. Not many bloodlines left, screening for magic potential, broom test.\"");
+			myLine = scan.nextLine();
+			System.out.println("\"Broom test?\"");
+			myLine = scan.nextLine();
+			System.out.println("\"yeah like in that scene in harry potter with the brooms. even being to call a broom to yourself was impressive. I was like one of three that could. Cant really do it anymore. Old magics gone.\"");
+			myLine = scan.nextLine();
+			System.out.println("\">:O\"");
+			myLine = scan.nextLine();
+			System.out.println("\"Did they not go over that in the kids class?\"");
+			myLine = scan.nextLine();
+			System.out.println("\"NoO! None of that! I never saw you do any of that stuff.\"");
+			myLine = scan.nextLine();
+			System.out.println("\"Oh yeah, no, of course not. They had the Grand Witch there just to amplify any nearby magic and still few of us could only summon a broom. That was about it. It was cool, but uneventful. When i got home i could barely knock over a broom from a distance\"");
+			myLine = scan.nextLine();
+			System.out.println("\">:O\"");
+			myLine = scan.nextLine();
+			System.out.println("\"Didnt mom take you bakc the next year ot learn all this stuff?\"");
+			myLine = scan.nextLine();
+			System.out.println("\"NoooOO!\"");
+			myLine = scan.nextLine();
+			System.out.println("\"Oh. welp. exposition.\"");
+			myLine = scan.nextLine();
+			System.out.println("What next?\n 1-Ask Mom\n 2-Take more time to proccess this\n 3-ah don beleve it");
+			myInteger = scan.nextInt();
+			
+			if(myInteger == 1)
+			{
+				System.out.println("\"hey so whats this about magic\"");
+				myLine = scan.nextLine();
+				System.out.println("\"oh yeah, the grand witch will be missed deerly!\"");
+				myLine = scan.nextLine();
+				System.out.println("\"No! why is this the irst time im herring about this?\"");
+				myLine = scan.nextLine();
+				System.out.println("\"What do you mean? Didnt you go to whtich camp forever ago?\"");
+				myLine = scan.nextLine();
+				System.out.println("\"No!\"");
+				myLine = scan.nextLine();
+				System.out.println("\"Oh. well if you want to get up to date and still try magic stuff, theyre taking interns right now to try to reigntie the well of magic\"");
+				myLine = scan.nextLine();
+				System.out.println("\"Really! Who do I speak to about that?\"");
+				myLine = scan.nextLine();
+				System.out.println("\"The high council. Here, Gretchen owes me a favor anyway. Ill call them up. you should meet them in the back of the fabric store downtown. \"");
+				myLine = scan.nextLine();
+				System.out.println("What next?\n 1-Go downtown\n");
+				myInteger = scan.nextInt();
+				
+				if(myInteger == 1)//roll for internship
+				{
+					System.out.println("\"Hello, im here for the internship.\"");
+					myLine = scan.nextLine();
+					System.out.println("\"Ah yes, your mother told me i should expect you. Your first so its all yours. Not many left even interested in this kind of thing. ANwyaways, the firs thing you need to pickup is 18k in perfume. we're no paying for it, figure it out.\"");
+					myLine = scan.nextLine();
+					System.out.println("\"Why perfume?\"");
+					myLine = scan.nextLine();
+					System.out.println("\"okay, so the grand witch had a familiar.\"");
+					myLine = scan.nextLine();
+					System.out.println("\"yes\"");
+					myLine = scan.nextLine();
+					System.out.println("\"and this familliar was just a whale she had enchanted\"");
+					myLine = scan.nextLine();
+					System.out.println("*Gives tedious look*");
+					myLine = scan.nextLine();
+					System.out.println("\"but its got hunted by wailers and was made into various goods and sold off peice by peice. And now all thats left is its ambergris which was used to make this case of perfumes, but that should be good enough\"");
+					myLine = scan.nextLine();
+					System.out.println("\"and so you want me to steal $18k worth of perfume\"");
+					myLine = scan.nextLine();
+					System.out.println("\"You dont have to steal it.\"");
+					myLine = scan.nextLine();
+					System.out.println("\"Beacue theres a little bit if magic whale in it?\"");
+					myLine = scan.nextLine();
+					System.out.println("\"yes, its being shipped to a perfume store about 8 miles from here\"");
+					myLine = scan.nextLine();
+					System.out.println("*At the perfume store*");
+					myLine = scan.nextLine();
+					System.out.println("\"The Truck with the shipment is still loaded, May as well just steal the whole truck.\"");
+					myLine = scan.nextLine();
+					///////////////Enter fight 
+					
+					///////////////win fight
+					System.out.println("*At the fabric store*");
+					myLine = scan.nextLine();
+					System.out.println("\"Very good, now your next job is to retrieve the Ruby Kicks from the grand witches hoard\"");
+					myLine = scan.nextLine();
+					System.out.println("\"which is where? and what?\"");
+					myLine = scan.nextLine();
+					System.out.println("\"She kept a storage unit down town, the shoes are prbably there\"");
+					myLine = scan.nextLine();
+					System.out.println("*At the storage units*");
+					myLine = scan.nextLine();
+					//roll, 1-20, if x>15
+					//roll, 1-20, if x>10
+					//roll, 1-20, if x>5 fight anyway
+					//fight
+					
+					System.out.println("*opens and searches storage unit*");
+					myLine = scan.nextLine();
+					System.out.println("*back at the fabric store*");
+					myLine = scan.nextLine();
+					System.out.println("\"Wow, these are nicer than i remember. \"");
+					myLine = scan.nextLine();
+					System.out.println("\"That was alot of trouble for some shoes\"");
+					myLine = scan.nextLine();
+					System.out.println("\"Okay, your next item is the piece of wand that was stolen from teh grand witch by the science teacher.\"");
+					myLine = scan.nextLine();
+					System.out.println("\"A science teacher? \"");
+					myLine = scan.nextLine();
+					System.out.println("\"Yeah, its partially petrified its so old. He shows it off to his student when they learn about fossils\"");
+					myLine = scan.nextLine();
+					System.out.println("\"?\"");
+					myLine = scan.nextLine();
+					System.out.println("\"Its probably in his desk as the school. good luck\"");
+					myLine = scan.nextLine();
+					System.out.println("*Tredding the halls of the school*");
+					myLine = scan.nextLine();
+					System.out.println("*from the shadows a man approaches*");
+					myLine = scan.nextLine();
+					//ta fight ensues
+					System.out.println("\"Wheres the petrifided wood?\"");
+					myLine = scan.nextLine();
+					System.out.println("\"Youll never find it. He'll never let you have it.\"");
+					myLine = scan.nextLine();
+					System.out.println("\"!\"");
+					myLine = scan.nextLine();
+					//st fight ensues
+					System.out.println("*back at the fabric store*");
+					myLine = scan.nextLine();
+					System.out.println("\"Alright! Now to reignite the well of magic, place the items in the well. And recite the incantation.\"");
+					myLine = scan.nextLine();
+					System.out.println("\"TadaSimSalaBimShazamHocusPocusAlakazamOpenSesame\"");
+					myLine = scan.nextLine();
+					System.out.println("Game Win");
+					myLine = scan.nextLine();
+					//winscreen
+					
+				}
+				else if(myInteger == 2)
+				{
+					System.out.println("\"Hello, im here for the internship.\"");
+					myLine = scan.nextLine();
+					System.out.println("\"sorry its been taken.\"");
+					myLine = scan.nextLine();
+					//bad end
+				}
+			}
+			else if(myInteger == 2)
+			{
+				System.out.println("*get busy with something else and forgets about it for 20 years*");
+				myLine = scan.nextLine();
+				//bad end
+			}
+			else if(myInteger == 3)
+			{
+				System.out.println("\"That was alot of informtion to make up right on the spot. You almost had me going there.\"");
+				myLine = scan.nextLine();
+				System.out.println("\"K\"");
+				myLine = scan.nextLine();
+				//bad end
+			}
+			//System.out.println("");
+			//myLine = scan.nextLine();
+		}
 		else if(myInteger == 2)
 		{
 			System.out.println("\"Is anybody there?\"");
@@ -26,7 +199,7 @@ public class Gam
 			myLine = scan.nextLine();
 			System.out.println("(You remember the voice, but it'll fade from your memory)");
 			myLine = scan.nextLine();
-			System.out.println("Ending A");
+			System.out.println("Flowers for [A]ndroids");
 		}
 		else if(myInteger == 3)
 		{
@@ -36,10 +209,11 @@ public class Gam
 			myLine = scan.nextLine();
 			System.out.println("(you forget about the voice.)");
 			myLine = scan.nextLine();
-			System.out.println("Ending B");
+			System.out.println("Meaningless [C]ode");
 		}
       
 		// Closing the scanner
 		scan.close();
 	}
 }
+
